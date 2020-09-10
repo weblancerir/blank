c19ed36bc45c69b51312c0becad1e369c72207d7
unknown <behrooz.arabzade@gmail.com>
2020-09-10 15:17:50 +0430

    remove build from gitignore

---
+++ build/precache-manifest.28d2a6ddf6e309c306c1fcd77a8f8f4b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14011d89278a5e26699217d15f9a398",
+    "url": "/%7Bhomepage%7D/index.html"
+  },
+  {
+    "revision": "42015faecb9457c2b7a4",
+    "url": "/%7Bhomepage%7D/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "a2ea5b5d25f8e8a775ad",
+    "url": "/%7Bhomepage%7D/static/css/main.814c832b.chunk.css"
+  },
+  {
+    "revision": "42015faecb9457c2b7a4",
+    "url": "/%7Bhomepage%7D/static/js/2.323d4cac.chunk.js"
+  },
+  {
+    "revision": "ed62fa59297061ed8277c0342618e0ad",
+    "url": "/%7Bhomepage%7D/static/js/2.323d4cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ea5b5d25f8e8a775ad",
+    "url": "/%7Bhomepage%7D/static/js/main.00098beb.chunk.js"
+  },
+  {
+    "revision": "b6c9a20d5df8874f9ce5",
+    "url": "/%7Bhomepage%7D/static/js/runtime-main.73a77473.js"
+  },
+  {
+    "revision": "b15149dfd3a5c8897f96a2059fbc2df1",
+    "url": "/%7Bhomepage%7D/static/media/align-bottom.b15149df.svg"
+  },
+  {
+    "revision": "c06baa7234abe1e072c006f05080220c",
+    "url": "/%7Bhomepage%7D/static/media/align-center.c06baa72.svg"
+  },
+  {
+    "revision": "0a5b083d2ce9a139b3daae794e2b55cc",
+    "url": "/%7Bhomepage%7D/static/media/align-left.0a5b083d.svg"
+  },
+  {
+    "revision": "18bded087757ee6d7107e3a33de52652",
+    "url": "/%7Bhomepage%7D/static/media/align-middle.18bded08.svg"
+  },
+  {
+    "revision": "47dacf6115af3c4e21ad59cada27eb76",
+    "url": "/%7Bhomepage%7D/static/media/align-right.47dacf61.svg"
+  },
+  {
+    "revision": "a4e81be5d8197a891f6accc6a3ca019e",
+    "url": "/%7Bhomepage%7D/static/media/align-top.a4e81be5.svg"
+  },
+  {
+    "revision": "91ca2215ba3799dd12b499bcad89853c",
+    "url": "/%7Bhomepage%7D/static/media/chain.91ca2215.svg"
+  },
+  {
+    "revision": "ebf2f3f4fa6920c7ea512bd4c2cca145",
+    "url": "/%7Bhomepage%7D/static/media/close.ebf2f3f4.svg"
+  },
+  {
+    "revision": "c16a073ead87cfa48143509c3762a1ef",
+    "url": "/%7Bhomepage%7D/static/media/maximize.c16a073e.svg"
+  },
+  {
+    "revision": "d1b14794c582938226afdf2b04a4f18d",
+    "url": "/%7Bhomepage%7D/static/media/minimize.d1b14794.svg"
+  },
+  {
+    "revision": "7e1c1c4d7851b4c467abec40f83230e1",
+    "url": "/%7Bhomepage%7D/static/media/more.7e1c1c4d.svg"
+  },
+  {
+    "revision": "b4611be47b0c32981c12ad17ddc7b0c8",
+    "url": "/%7Bhomepage%7D/static/media/paint.b4611be4.svg"
+  },
+  {
+    "revision": "4ffb5b5d6464042179b2eb0dad535550",
+    "url": "/%7Bhomepage%7D/static/media/plus.4ffb5b5d.svg"
+  },
+  {
+    "revision": "910508a9542d41bd344c469c3974ef6d",
+    "url": "/%7Bhomepage%7D/static/media/question.910508a9.svg"
+  },
+  {
+    "revision": "91af39196e22ae67e243eab414db9dcf",
+    "url": "/%7Bhomepage%7D/static/media/unlink.91af3919.svg"
+  }
+]);
